f921aadb9969bbdbf92117e5150c3bdb196a2866
Gaurav-2111 <gauravdev339@gmail.com>
2026-07-06 23:26:17 +0530

    improved main file

--- projects/main.py
+++ projects/main.py
@@ -1,3 +1,4 @@
+#main.py to run the pipeline
 all_data = []
 for i in range(1,4):
   params = {
@@ -5,4 +6,16 @@ for i in range(1,4):
     "page": i
   }
   movies = extract_data(endpoint,params)
+  if movies is None:
+    raise RuntimeError("pipeline stopped because data extraction failed")
+
   all_data.extend(movies['results'])
+
+# calling save data with the file_name
+today = date.today()
+file_name = today.strftime("%Y-%m-%d")
+save_data(all_data,file_name)
+
+#calling the transform function
+transform_data(all_data)
+
