dde35889b939f5227fd51e09509228ad6b2ec1dd
Gaurav-2111 <gauravdev339@gmail.com>
2026-07-11 23:53:49 +0530

    added a load function which will send my data to database

---
+++ projects/load.py
@@ -0,0 +1,68 @@
+#load data
+def load_data(clean_data):
+
+  connection = mysql.connector.connect(
+      host=DB_HOST,
+      user=DB_USER,
+      password=DB_PASSWORD,
+      database=DB_NAME
+    )
+  # cursor is going to connect our python with mysql
+  cursor = connection.cursor()
+
+  try:
+    create_table_query = """CREATE TABLE IF NOT EXISTS movies (
+        id INT PRIMARY KEY,
+        title VARCHAR(200) NOT NULL,
+        release_date date,
+        popularity FLOAT NOT NULL,
+        vote_average FLOAT NOT NULL,
+        vote_count INT NOT NULL,
+        original_language VARCHAR(10) NOT NULL,
+        adult BOOLEAN NOT NULL,
+        overview TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )"""
+
+    # this is so that our table is created
+    cursor.execute(create_table_query)
+
+
+    insert_query = """insert ignore into movies (id,title,release_date,popularity,vote_average,vote_count,original_language,adult,overview)
+      values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    
+    # creating a list because executemany() uses combined data as it process data in a batch
+    batch_data = []
+
+    for movie in clean_data:
+      
+
+        values = (
+              movie["id"],
+              movie["title"],
+              movie["release_date"],
+              movie["popularity"],
+              movie["vote_average"],
+              movie["vote_count"],
+              movie["original_language"],
+              movie["adult"],
+              movie["overview"]
+          )
+        # appending data of each row as a tuple inside a list 
+        # [(row 1),(row 2),(row 3)] every tuple is going to fill the place of those place holders we created in insert_query
+        batch_data.append(values)
+
+    # executemany to send all data at once 
+    cursor.executemany(insert_query,batch_data)
+    connection.commit()
+
+  # if there was a error in storing data in database
+  except Exception as e:
+    print(f"database transaction failed so rolling back changes , error : {e}")
+    connection.rollback()
+
+  # it does not matter what happened we always close are connectoin
+  finally:
+    cursor.close()
+    connection.close()
+
