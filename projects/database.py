af2a076168605fb7070a3c1e123b1aace53c3ed6
Gaurav-2111 <gauravdev339@gmail.com>
2026-07-12 22:15:42 +0530

    done

--- projects/database.py
+++ projects/database.py
@@ -1,10 +1,13 @@
+import os
 import mysql.connector
-from config import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='1234',
-        database='movies_db'
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
